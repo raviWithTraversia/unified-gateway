1ff058c41d1075e476f8763e64aa8e01ede53ae4
aali@traversia.net <aali@traversia.net>
2023-12-20 11:53:04 +0530

    20-12-23

--- controllers/flight/flight.service.js
+++ controllers/flight/flight.service.js
@@ -183,7 +183,7 @@ async function handleflight(
     })
   );
 
-  
+
   // Combine the responses here
   const combineResponseObj = {};
   supplierCredentials.forEach((supplier, index) => {
@@ -269,22 +269,45 @@ const internationalKafilaFun = async (
   }
   
   let tripTypeValue;
-
-  switch (TypeOfTrip) {
-    case "ONEWAY":
-      tripTypeValue = "I1";
-      break;
-    case "ROUNDTRIP":
-      tripTypeValue = "I2";
-      break;
-    case "MULTYCITY":
-      tripTypeValue = "I3";
-      break;
-    default:
-      return {
-        IsSucess: false,
-        response: "Invalid TypeOfTrip",
-      };
+  if (TravelType == "International") {
+    switch (TypeOfTrip) {
+      case "ONEWAY":
+        tripTypeValue = "I1";
+        break;
+      case "ROUNDTRIP":
+        tripTypeValue = "I2";
+        break;
+      case "MULTYCITY":
+        tripTypeValue = "I3";
+        break;
+      default:
+        return {
+          IsSucess: false,
+          response: "Invalid TypeOfTrip",
+        };
+    }
+  }else if(TravelType == "Domestic"){
+    switch (TypeOfTrip) {
+      case "ONEWAY":
+        tripTypeValue = "D1";
+        break;
+      case "ROUNDTRIP":
+        tripTypeValue = "D2";
+        break;
+      case "MULTYCITY":
+        tripTypeValue = "D3";
+        break;
+      default:
+        return {
+          IsSucess: false,
+          response: "Invalid TypeOfTrip",
+        };
+    }
+  }else{
+    return {
+      IsSucess: false,
+      response: "Invalid TypeOfTrip",
+    };
   } 
   //Class Of Service Economy, Business, Premium Economy
   const classOfServiceMap = {
@@ -364,119 +387,119 @@ const internationalKafilaFun = async (
       //flightCache.set(cacheKey, fSearchApiResponse.data, 300);
       let apiResponse = fSearchApiResponse.data;
       let apiResponseCommon = [];
-      for (let schedule of apiResponse.Schedules[0]) { 
-        let randomUID = uuid.v4();
-        //apiResponseCommon.push(schedule);
-        apiResponseCommon.push({
-          "UID": randomUID,
-          "BaseFare":schedule.Fare.BasicTotal,
-          "Taxes":schedule.Fare.TaxesTotal,
-          "TotalPrice":schedule.Fare.GrandTotal,
-          "ExtraCharges":0.0,
-          "TaxMarkUp":0.0,
-          "MarkUp":0.0,
-          "Commission":0.0,
-          "Fees":0.0,
-          "BookingFees":0.0,
-          "ServiceFee":0.0,
-          "CancellationFees":0.0,
-          "RescheduleFees":0.0,
-          "AdminFees":0.0,
-          "Discount":0.0,
-          "TDS":0.0,
-          "BaseCharges":0.0,
-          "SupplierDiscount":0.0,
-          "SupplierDiscountPercent":0.0,
-          "GrandTotal":schedule.Fare.GrandTotal,
-          "Currency":"INR",
-          "FareType":"",
-          "TourCode":"",
-          "PricingMethod":"Guaranteed",
-          "FareFamily":"Regular Fare",
-          "PromotionalFare":false,
-          "FareFamilyDN":null,
-          "PromotionalCode":"",
-          "PromoCodeType":"Partner",
-          "RefundableFare":false,
-          "IndexNumber":0,
-          "Provider":"1G",
-          "ValCarrier":"AI",
-          "LastTicketingDate":"2023-12-27T23:59:00.000+05:30",
-          "TravelTime":null,
-          "Sectors":schedule.Itinerary.map(sector => (
-            {
-              "IsConnect":false,
-              "AirlineCode": sector.FCode,
-              "AirlineName": sector.FName,
-              "Class": sector.FClass,
-              "CabinClass":"Economy",
-              "BookingCounts":"",
-              "NoSeats":0,
-              "FltNum":sector.FNo,
-              "EquipType":"32N",
-              "FlyingTime":"01:25",
-              "TravelTime":"03:30",
-              "TechStopOver":1,
-              "Status":"",
-              "OperatingCarrier":null,
-              "MarketingCarrier":null,
-              "BaggageInfo":"20 Kilograms",
-              "HandBaggage":"7 KG",
-              "TransitTime":null,
-              "MealCode":null,
-              "Key":"gvEvQxFDuDKAOTyacNAAAA==",
-              "Distance":"708",
-              "ETicket":"Yes",
-              "ChangeOfPlane":"false",
-              "ParticipantLevel":"Secure Sell",
-              "OptionalServicesIndicator":false,
-              "AvailabilitySource":"P",
-              "Group":"0",
-              "LinkAvailability":"true",
-              "PolledAvailabilityOption":"Cached status used. Polled avail exists",
-              "FareBasisCode":"SIP",
-              "HostTokenRef":"",
-              "APISRequirementsRef":"",
-              "Departure":{
-                "Terminal":sector.DTrmnl,
-                "Date": sector.DDate.split('T')[0],
-                "Time":sector.DDate.split('T')[1].substring(0, 5),
-                "Day":null,
-                "DateTimeStamp":sector.DDate,
-                "Code":sector.Src,
-                "Name":sector.DArpt,
-                "CityCode":sector.Src,
-                "CityName": sector.SrcName,
-                "CountryCode":"IN",
-                "CountryName":"India"
-              },
-              "Arrival":{
-                "Terminal":sector.ATrmnl,
-                "Date":sector.ADate.split('T')[0],
-                "Time":sector.ADate.split('T')[1].substring(0, 5),
-                "Day":null,
-                "DateTimeStamp":sector.ADate,
-                "Code":sector.Des,
-                "Name":sector.AArpt,
-                "CityCode":sector.Des,
-                "CityName":sector.DesName,
-                "CountryCode":"IN",
-                "CountryName":"India"
-              }
-            }
-            )),
-          "HostTokens":null,
-          "Key":"gvEvQxFDuDKAkayacNAAAA==",
-          "SearchID":"2a1aefe1-8b53-419a-b5fb-44a7f4b4f859",
-          "TRCNumber":null,
-          "TraceId":"c27f4b59-679c-47c4-922e-8ca4db457c3a"  
-        });
-      }
+      // for (let schedule of apiResponse.Schedules[0]) { 
+      //   let randomUID = uuid.v4();
+      //   apiResponseCommon.push(schedule);
+      //   apiResponseCommon.push({
+      //     "UID": randomUID,
+      //     "BaseFare":schedule.Fare.BasicTotal,
+      //     "Taxes":schedule.Fare.TaxesTotal,
+      //     "TotalPrice":schedule.Fare.GrandTotal,
+      //     "ExtraCharges":0.0,
+      //     "TaxMarkUp":0.0,
+      //     "MarkUp":0.0,
+      //     "Commission":0.0,
+      //     "Fees":0.0,
+      //     "BookingFees":0.0,
+      //     "ServiceFee":0.0,
+      //     "CancellationFees":0.0,
+      //     "RescheduleFees":0.0,
+      //     "AdminFees":0.0,
+      //     "Discount":0.0,
+      //     "TDS":0.0,
+      //     "BaseCharges":0.0,
+      //     "SupplierDiscount":0.0,
+      //     "SupplierDiscountPercent":0.0,
+      //     "GrandTotal":schedule.Fare.GrandTotal,
+      //     "Currency":"INR",
+      //     "FareType":"",
+      //     "TourCode":"",
+      //     "PricingMethod":"Guaranteed",
+      //     "FareFamily":"Regular Fare",
+      //     "PromotionalFare":false,
+      //     "FareFamilyDN":null,
+      //     "PromotionalCode":"",
+      //     "PromoCodeType":"Partner",
+      //     "RefundableFare":false,
+      //     "IndexNumber":0,
+      //     "Provider":"1G",
+      //     "ValCarrier":"AI",
+      //     "LastTicketingDate":"2023-12-27T23:59:00.000+05:30",
+      //     "TravelTime":null,
+      //     "Sectors":schedule.Itinerary.map(sector => (
+      //       {
+      //         "IsConnect":false,
+      //         "AirlineCode": sector.FCode,
+      //         "AirlineName": sector.FName,
+      //         "Class": sector.FClass,
+      //         "CabinClass":"Economy",
+      //         "BookingCounts":"",
+      //         "NoSeats":0,
+      //         "FltNum":sector.FNo,
+      //         "EquipType":"32N",
+      //         "FlyingTime":"01:25",
+      //         "TravelTime":"03:30",
+      //         "TechStopOver":1,
+      //         "Status":"",
+      //         "OperatingCarrier":null,
+      //         "MarketingCarrier":null,
+      //         "BaggageInfo":"20 Kilograms",
+      //         "HandBaggage":"7 KG",
+      //         "TransitTime":null,
+      //         "MealCode":null,
+      //         "Key":"gvEvQxFDuDKAOTyacNAAAA==",
+      //         "Distance":"708",
+      //         "ETicket":"Yes",
+      //         "ChangeOfPlane":"false",
+      //         "ParticipantLevel":"Secure Sell",
+      //         "OptionalServicesIndicator":false,
+      //         "AvailabilitySource":"P",
+      //         "Group":"0",
+      //         "LinkAvailability":"true",
+      //         "PolledAvailabilityOption":"Cached status used. Polled avail exists",
+      //         "FareBasisCode":"SIP",
+      //         "HostTokenRef":"",
+      //         "APISRequirementsRef":"",
+      //         "Departure":{
+      //           "Terminal":sector.DTrmnl,
+      //           "Date": sector.DDate.split('T')[0],
+      //           "Time":sector.DDate.split('T')[1].substring(0, 5),
+      //           "Day":null,
+      //           "DateTimeStamp":sector.DDate,
+      //           "Code":sector.Src,
+      //           "Name":sector.DArpt,
+      //           "CityCode":sector.Src,
+      //           "CityName": sector.SrcName,
+      //           "CountryCode":"IN",
+      //           "CountryName":"India"
+      //         },
+      //         "Arrival":{
+      //           "Terminal":sector.ATrmnl,
+      //           "Date":sector.ADate.split('T')[0],
+      //           "Time":sector.ADate.split('T')[1].substring(0, 5),
+      //           "Day":null,
+      //           "DateTimeStamp":sector.ADate,
+      //           "Code":sector.Des,
+      //           "Name":sector.AArpt,
+      //           "CityCode":sector.Des,
+      //           "CityName":sector.DesName,
+      //           "CountryCode":"IN",
+      //           "CountryName":"India"
+      //         }
+      //       }
+      //       )),
+      //     "HostTokens":null,
+      //     "Key":"gvEvQxFDuDKAkayacNAAAA==",
+      //     "SearchID":"2a1aefe1-8b53-419a-b5fb-44a7f4b4f859",
+      //     "TRCNumber":null,
+      //     "TraceId":"c27f4b59-679c-47c4-922e-8ca4db457c3a"  
+      //   });
+      // }
 
 
       return {
         IsSucess: true,
-        response: apiResponseCommon,
+        response: apiResponse,
       };     
          
      

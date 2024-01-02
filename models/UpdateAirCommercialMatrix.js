6020e3f41a1ed52fb099700e87fd1e26383498a4
= <111alam567@gmail.com>
2024-01-02 10:09:24 +0530

    change into commercial matrix

--- models/UpdateAirCommercialMatrix.js
+++ models/UpdateAirCommercialMatrix.js
@@ -1,63 +1,41 @@
 const mongoose = require('mongoose');
-
+const Ajv = require('ajv');
 const { Schema } = mongoose;
 
-
 const UpdateAirCommercialMatrixSchema = new Schema({
-  comercialPlanId: { 
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'CommercialAirPlan'
+  comercialPlanId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'CommercialAirPlan'
   },
   airCommercialPlanId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'AirCommercial'
-    },
-  ComanyId: {
+  },
+  companyId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Company'
-     },
-  rateValue: [{
-    rowId: {
+  },
+  data: [{
+    AirCommertialRowMasterId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'AirCommertialColumnMaster'
     },
-    coloumn: [{
-      coloumnId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'AirCommertialColumnMaster'
+    AirCommertialColumnMasterId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'AirCommertialColumnMaster'
+    },
+    value: {
+      type: 'object',
+      properties: {
+        numberValue: { type: 'number' },
+        stringValue: { type: 'string' },
+        booleanValue: { type: 'boolean' }
       },
-      type: {
-        type: String,
-      },
-      value: {
-        type: String
-      },
-      textType: {
-        type: String
-      }
-    }]
+    },
+    textType: {
+      type: String
+    }
   }],
-  fixedValue: [{
-    rowId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'AirCommertialColumnMaster'
-    },
-    coloumn: [{
-      coloumnId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'AirCommertialColumnMaster'
-      },
-      type: {
-        type: String,
-      },
-      value: {
-        type: String
-      },
-      textType: {
-        type: String
-      }
-    }]
-  }]
 });
 
 module.exports = mongoose.model('UpdateAirCommercialMatrix', UpdateAirCommercialMatrixSchema);

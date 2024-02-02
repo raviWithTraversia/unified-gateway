08c4213a17ffa3e22d82b26242b62c120e76a98a
Shashi-Kr-Singh <skumar@traversia.net>
2024-02-02 17:24:18 +0530

    fix registration

--- controllers/registration/registration.services.js
+++ controllers/registration/registration.services.js
@@ -120,8 +120,9 @@ const addRegistration = async (req, res) => {
       mailConfig = await Smtp.findById(id);
     }
     let checkCompanyType = await companyModels.findById(companyId);
+    let parent = null
     if(checkCompanyType.type === "Distributer"){
-      parent = checkCompanyType?.parent || null
+       parent = checkCompanyType?.parent || null
     }
     const newRegistration = new registration({
       companyId,
@@ -149,10 +150,10 @@ const addRegistration = async (req, res) => {
       gstState: gstState || null,
       gstPinCode: gstPinCode || null,
       agencyGroupId,
-      parant : parent || null
+      parant : companyId
     });
     let newRegistrationRes = await newRegistration.save();
-    console.log(newRegistrationRes);
+   // console.log(newRegistrationRes);
     let mailText = newRegistrationRes;
     let mailSubject = `New registration created successfully`;
     let smsUrl = await configCred.findOne({ companyId: companyId });

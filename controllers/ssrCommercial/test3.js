8fb1f800c48dd977c65f31869a1c416fbe76e4d4
Shashi-Kr-Singh <skumar@traversia.net>
2024-02-06 11:00:59 +0530

    add key in ssrCommercialGroup

--- controllers/ssrCommercial/test3.js
+++ controllers/ssrCommercial/test3.js
@@ -1,3 +1,3 @@
-let obj1 = {
+let seat = {
     
 }
--- routes/ssrCommercialGroupRoute.js
+++ routes/ssrCommercialGroupRoute.js
@@ -2,37 +2,35 @@ const express = require("express");
 const ssrCommercialGroup_route = express();
 const bodyParser = require("body-parser");
 ssrCommercialGroup_route.use(bodyParser.json());
-ssrCommercialGroup_route.use(bodyParser.urlencoded({extended:true}));
+ssrCommercialGroup_route.use(bodyParser.urlencoded({ extended: true }));
 const auth = require("../middleware/auth");
-const ssrCommercialGroupController = require('../controllers/ssrCommercialGroup/ssrCommercialGroup.controller');
+const ssrCommercialGroupController = require("../controllers/ssrCommercialGroup/ssrCommercialGroup.controller");
 
 ssrCommercialGroup_route.post(
-    '/ssrCommercialGroup/addSsrCommercialGroup',
-    auth,
-    ssrCommercialGroupController.addSsrCommercialGroup
+  "/ssrCommercialGroup/addSsrCommercialGroup",
+  auth,
+  ssrCommercialGroupController.addSsrCommercialGroup
 );
 
 ssrCommercialGroup_route.get(
-    '/ssrCommercialGroup/getSsrCommercialGroup',
-     auth,
-    ssrCommercialGroupController.getSsrCommercialGroup
+  "/ssrCommercialGroup/getSsrCommercialGroup",
+  auth,
+  ssrCommercialGroupController.getSsrCommercialGroup
 );
 ssrCommercialGroup_route.patch(
-    '/ssrCommercialGroup/editSsrCommercialGroup',
-     auth,
-    ssrCommercialGroupController.editSsrCommercialGroup
+  "/ssrCommercialGroup/editSsrCommercialGroup",
+  auth,
+  ssrCommercialGroupController.editSsrCommercialGroup
 );
 
 ssrCommercialGroup_route.delete(
-    '/ssrCommercialGroup/deleteSsrCommercialGroup',
-     auth,
-    ssrCommercialGroupController.deleteSsrCommercialGroup
+  "/ssrCommercialGroup/deleteSsrCommercialGroup",
+  auth,
+  ssrCommercialGroupController.deleteSsrCommercialGroup
 );
 
-
-
-ssrCommercialGroup_route.get('/test',auth, function(req, res){
-    res.status(200).json({status:"success",msg:"this is test responce"});
+ssrCommercialGroup_route.get("/test", auth, function (req, res) {
+  res.status(200).json({ status: "success", msg: "this is test responce" });
 });
 
 module.exports = ssrCommercialGroup_route;

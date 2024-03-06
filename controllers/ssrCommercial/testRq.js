2b9cd04d82965c2aa0dc55f2a6f512e39a16fdbb
Shashi-Kr-Singh <skumar@traversia.net>
2024-03-06 12:51:03 +0530

    fix series booking

--- controllers/ssrCommercial/testRq.js
+++ controllers/ssrCommercial/testRq.js
@@ -34,6 +34,156 @@ let reqObj =  {
     "ValCarrier": "IX",
     "LastTicketingDate": "",
     "TravelTime": "",
+    "Error": {
+        "Status": null,
+        "Result": null,
+        "ErrorMessage": "",
+        "ErrorCode": null,
+        "Location": "SELL",
+        "WarningMessage": "",
+        "IsError": false,
+        "IsWarning": false
+    },
+    "IsFareUpdate": false,
+    "IsAncl": false,
+    "Param": {
+        "Trip": "D1",
+        "Adt": 2,
+        "Chd": 0,
+        "Inf": 0,
+        "Sector": [
+            {
+                "Src": "DEL",
+                "Des": "BLR",
+                "DDate": "2024-03-25"
+            }
+        ],
+        "PF": "",
+        "PC": "",
+        "Routing": "ALL",
+        "Ver": "1.0.0.0",
+        "Auth": {
+            "AgentId": "675923",
+            "Token": "fd58e3d2b1e517f4ee46063ae176eee1"
+        },
+        "Env": "D",
+        "Module": "B2B",
+        "OtherInfo": {
+            "PromoCode": "",
+            "FFlight": "",
+            "FareType": "",
+            "TraceId": "65d47d09df6e2927826a9bb7",
+            "IsUnitTesting": false,
+            "TPnr": false,
+            "FAlias": null,
+            "IsLca": false
+        }
+    },
+    "SelectedFlight": {
+        "PId": 0,
+        "Id": 0,
+        "TId": 0,
+        "Src": "DEL",
+        "Des": "BLR",
+        "FCode": "6E",
+        "FName": "GoIndigo",
+        "FNo": "2375",
+        "DDate": "2024-03-25T03:00:00",
+        "ADate": "2024-03-25T05:50:00",
+        "Dur": "0d:2h:50m",
+        "Stop": "0",
+        "Seat": 88,
+        "Sector": "DEL,BLR",
+        "Itinerary": [
+            {
+                "Id": 0,
+                "Src": "DEL",
+                "SrcName": "Delhi",
+                "Des": "BLR",
+                "DesName": "Bengaluru",
+                "FLogo": "0 -123px",
+                "FCode": "6E",
+                "FName": "GoIndigo",
+                "FNo": "2375",
+                "DDate": "2024-03-25T03:00:00",
+                "ADate": "2024-03-25T05:50:00",
+                "DTrmnl": "2",
+                "ATrmnl": "1",
+                "DArpt": "Delhi Indira Gandhi Intl",
+                "AArpt": "Bengaluru Intl Arpt",
+                "Dur": "0d:2h:50m",
+                "layover": "",
+                "Seat": 88,
+                "FClass": "R",
+                "PClass": "R",
+                "FBasis": "R0IP",
+                "FlightType": "321",
+                "OI": [
+                    "SSK=6E~2375~ ~~DEL~03/25/2024 03:00~BLR~03/25/2024 05:50~~"
+                ]
+            }
+        ],
+        "Fare": {
+            "GrandTotal": 2217,
+            "BasicTotal": 1566,
+            "YqTotal": 0,
+            "TaxesTotal": 651,
+            "Adt": {
+                "Basic": 1566,
+                "Yq": 0,
+                "Taxes": 651,
+                "Total": 2217
+            },
+            "Chd": null,
+            "Inf": null,
+            "OI": [
+                {
+                    "FAT": null,
+                    "FSK": "MH5Yfn5JNX5YTkJBMDAwfk5CMDB_fjB_Nn5_WCEw"
+                }
+            ]
+        },
+        "FareRule": {
+            "CBNBG": "7KG",
+            "CHKNBG": "15KG",
+            "CBH": "96HRS",
+            "CWBH": "96HRS-4HRS",
+            "RBH": "96HRS",
+            "RWBH": "96HRS-4HRS",
+            "CBHA": 3000,
+            "CWBHA": 3600,
+            "RBHA": 2850,
+            "RWBHA": 3350,
+            "SF": 50
+        },
+        "Alias": "MAIN",
+        "FareType": null,
+        "PFClass": "A-R",
+        "Offer": {
+            "Msg": "",
+            "Refund": "",
+            "IsPromoAvailable": true,
+            "IsGstMandatory": false,
+            "IsLcc": true
+        },
+        "OI": {
+            "Jsk": "SVh_IDc4NH4gfn5ERUx_MDQvMjgvMjAyNCAxNjozNX5MS09_MDQvMjgvMjAyNCAxNzozNX5_",
+            "Pcc": "49354F5441494E4B4146494C447E4D41494E",
+            "Security": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkb3RSRVogQVBJIiwianRpIjoiMjlhNzhkZDAtOTEyZS03YzBiLWM1MGYtNDUxMDg2ZjUxMjlhIiwiaXNzIjoiZG90UkVaIEFQSSJ9.H2S04zCoNdFRYxmhgourbqH_dHHgQ3xdU2isKbDWDm0"
+        },
+        "Deal": {
+            "NETFARE": 13439,
+            "TDISC": 553,
+            "TDS": 28,
+            "GST": 100,
+            "DISCOUNT": {
+                "DIS": 553,
+                "SF": 0,
+                "PDIS": 0,
+                "CB": 0
+            }
+        }
+    },
     "PriceBreakup": [
         {
             "PassengerType": "ADT",
@@ -183,111 +333,66 @@ let reqObj =  {
         }
     ],
     "FareDifference": {
-        "FareDifference": 0,
-        "NewFare": 2217,
-        "OldFare": 2217,
+        "FareDifference": 0.0000,
+        "NewFare": 13006.0000,
+        "OldFare": 13006,
         "Journeys": [
             {
-                "Security": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkb3RSRVogQVBJIiwianRpIjoiMjlhNzhkZDAtOTEyZS03YzBiLWM1MGYtNDUxMDg2ZjUxMjlhIiwiaXNzIjoiZG90UkVaIEFQSSJ9.H2S04zCoNdFRYxmhgourbqH_dHHgQ3xdU2isKbDWDm0",
-                "IPaxkey": "MCFBRFQ-",
-                "TotalFare": 2217,
-                "BasicTotal": 1566,
+                "Security": "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5MmU0OTUzOS1hMTFlLTRjMTYtYmZhNy0zYWFhYmUwYjBhNjMiLCJzZXNzaW9uSUQiOiI5NjQzNjkzIiwiYWdlbnRJRCI6IjE4Mzk2OTU2IiwiZG9tYWluQ29kZSI6IldXVyIsImFnZW50TmFtZSI6Ik9USTAyNiIsIm9yZ2FuaXphdGlvbkNvZGUiOiJPVEkwMjYiLCJyb2xlQ29kZSI6IlJDVFQiLCJjaGFubmVsVHlwZSI6IkFQSSIsInN5c3RlbVR5cGUiOiJXZWJTZXJ2aWNlc0FQSSIsImNsaWVudE5hbWUiOiJXZWJTZXJ2aWNlc0FQSSIsImN1bHR1cmVDb2RlIjoiZW4tVVMiLCJjdXJyZW5jeUNvZGUiOiJJTlIiLCJsb2NhdGlvbkNvZGUiOiJXV1ciLCJwZXJzb25JRCI6IjE4NDI2NDk5IiwicGVyc29uVHlwZSI6IjIiLCJhZ2VudFF1ZXVlQ29kZSI6IiIsIm9yZ2FuaXphdGlvblF1ZXVlQ29kZSI6IiIsImV4cCI6MTc0MTA2NDk0MX0.bWBFlkNB4l4FNSM2cbrchy8k-q7L_UOU1oClELxM5BnZKNVQ2FAFnd4UauW_OIt2snC20Eph5bfa9r6O4GCq1DPutXhwCEwsIzITSOGKGxq2sfF6M16rYX4RQQD4IvZlsvh7G8HBSHh6sNp7qKzvLgBq0eHQh99m1lkZ1izF_Eq_-NubQeUwGTfxFKsTMzcAO4bi6dzrqWVnLkn56JnIIUyTvYhKUkBm7-ZAKdNKSAbzYoXclHFNrfidopj7VblTk7uo90FdYdE5JFFdXG5BKEgvaZ5E958r1JhgbKYqdawH31IKXE6xXJrbh8OcGP7dhuRJ4cqIx4If6CuY4qDegVXL-1kwMuLkCz6QYel8bR0hDjdkqZIDcw8t16SKhTR8k-eFgFbedC8lmo-h6QxkYNVArg1OLtwT8iyjH-CKXFPZRlb6daURQR3T1VEFCpCbwYRSeKLsD8jGTsGLpcPSmQw2SVCc41VCI2XQFa_onE8Xw1zhdXIpEQtZZxVBJGZL",
+                "IPaxkey": null,
+                "TotalFare": 13006.0000,
+                "BasicTotal": 11060.0000,
                 "YqTotal": 0,
-                "TaxTotal": 651,
+                "TaxTotal": 1946,
                 "Segments": [
                     {
                         "PaxType": "ADT",
                         "TaxBreakup": [
                             {
-                                "CType": "",
+                                "CType": "FarePrice",
                                 "CCode": "",
-                                "Amt": 1566
+                                "Amt": 5530.0000
                             },
                             {
-                                "CType": "CUTE",
-                                "CCode": "CUT",
-                                "Amt": 75
+                                "CType": "TravelFee",
+                                "CCode": "PHF",
+                                "Amt": 50
                             },
                             {
-                                "CType": "RCS",
-                                "CCode": "RCS",
-                                "Amt": 100
+                                "CType": "TravelFee",
+                                "CCode": "RCF",
+                                "Amt": 50
                             },
                             {
-                                "CType": "IN",
-                                "CCode": "IN",
-                                "Amt": 61
+                                "CType": "Tax",
+                                "CCode": "TTF",
+                                "Amt": 194
                             },
                             {
-                                "CType": "P2",
-                                "CCode": "P2",
+                                "CType": "TravelFee",
+                                "CCode": "ASF",
                                 "Amt": 236
                             },
                             {
-                                "CType": "WO",
-                                "CCode": "WO",
+                                "CType": "TravelFee",
+                                "CCode": "PSF",
                                 "Amt": 91
                             },
                             {
-                                "CType": "CGST",
-                                "CCode": "CST",
-                                "Amt": 44
+                                "CType": "TravelFee",
+                                "CCode": "UDF",
+                                "Amt": 61
                             },
                             {
-                                "CType": "SGST",
-                                "CCode": "SST",
-                                "Amt": 44
+                                "CType": "Tax",
+                                "CCode": "07GST",
+                                "Amt": 291
                             }
                         ]
                     }
                 ]
             }
         ]
-    },
-    "Error": {
-        "Status": null,
-        "Result": null,
-        "ErrorMessage": "",
-        "ErrorCode": null,
-        "Location": null,
-        "WarningMessage": "",
-        "IsError": false,
-        "IsWarning": false
-    },
-    "IsFareUpdate": false,
-    "IsAncl": false,
-    "Param": {
-        "Trip": "D1",
-        "Adt": 1,
-        "Chd": 0,
-        "Inf": 0,
-        "Sector": [
-            {
-                "Src": "DEL",
-                "Des": "LKO",
-                "DDate": "2024-04-28"
-            }
-        ],
-        "PF": "",
-        "PC": "",
-        "Routing": "ALL",
-        "Ver": "1.0.0.0",
-        "Auth": {
-            "AgentId": "675923",
-            "Token": "fd58e3d2b1e517f4ee46063ae176eee1"
-        },
-        "Env": "D",
-        "Module": "B2B",
-        "OtherInfo": {
-            "PromoCode": "",
-            "FFlight": "",
-            "FareType": "",
-            "TraceId": "65d47d09df6e2927826a9bb7",
-            "IsUnitTesting": false,
-            "TPnr": false,
-            "FAlias": null,
-            "IsLca": false
-        }
     },
     "GstData": {
         "IsGst": false,
@@ -300,109 +405,6 @@ let reqObj =  {
             "State": "Delhi",
             "Type": "",
             "Gstn": "07AAACD3853F1ZW"
-        }
-    },
-    "SelectedFlight": {
-        "PId": 0,
-        "Id": 0,
-        "TId": 0,
-        "Src": "DEL",
-        "Des": "LKO",
-        "FCode": "IX",
-        "FName": "",
-        "FNo": "",
-        "DDate": "",
-        "ADate": "",
-        "Dur": "",
-        "Stop": "",
-        "Seat": 9,
-        "Sector": "",
-        "Itinerary": [
-            {
-                "Id": 0,
-                "Src": "DEL",
-                "SrcName": "Delhi",
-                "Des": "LKO",
-                "DesName": "Lucknow",
-                "FLogo": "",
-                "FCode": "IX",
-                "FName": "AirIndiaExpress",
-                "FNo": "784",
-                "DDate": "2024-04-28",
-                "ADate": "2024-04-28",
-                "DTrmnl": "3",
-                "ATrmnl": "2",
-                "DArpt": "Delhi Indira Gandhi Intl",
-                "AArpt": "Amausi Arpt",
-                "Dur": "0d:1h:0m",
-                "layover": "",
-                "Seat": 0,
-                "FClass": "X",
-                "PClass": "BT",
-                "FBasis": "XNBA000",
-                "FlightType": "7M8",
-                "OI": null
-            }
-        ],
-        "Fare": {
-            "GrandTotal": 2217,
-            "BasicTotal": 1566,
-            "YqTotal": 0,
-            "TaxesTotal": 651,
-            "Adt": {
-                "Basic": 1566,
-                "Yq": 0,
-                "Taxes": 651,
-                "Total": 2217
-            },
-            "Chd": null,
-            "Inf": null,
-            "OI": [
-                {
-                    "FAT": null,
-                    "FSK": "MH5Yfn5JNX5YTkJBMDAwfk5CMDB_fjB_Nn5_WCEw"
-                }
-            ]
-        },
-        "FareRule": {
-            "CBNBG": "7KG",
-            "CHKNBG": "15KG",
-            "CBH": "96HRS",
-            "CWBH": "96HRS-4HRS",
-            "RBH": "96HRS",
-            "RWBH": "96HRS-4HRS",
-            "CBHA": 3000,
-            "CWBHA": 3600,
-            "RBHA": 2850,
-            "RWBHA": 3350,
-            "SF": 50
-        },
-        "Alias": "MAIN",
-        "FareType": null,
-        "PFClass": "A-R",
-        "Offer": {
-            "Msg": "",
-            "Refund": "",
-            "IsPromoAvailable": true,
-            "IsGstMandatory": false,
-            "IsLcc": true
-        },
-        "OI": {
-            "Jsk": "SVh_IDc4NH4gfn5ERUx_MDQvMjgvMjAyNCAxNjozNX5MS09_MDQvMjgvMjAyNCAxNzozNX5_",
-            "Pcc": "49354F5441494E4B4146494C447E4D41494E",
-            "Security": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkb3RSRVogQVBJIiwianRpIjoiMjlhNzhkZDAtOTEyZS03YzBiLWM1MGYtNDUxMDg2ZjUxMjlhIiwiaXNzIjoiZG90UkVaIEFQSSJ9.H2S04zCoNdFRYxmhgourbqH_dHHgQ3xdU2isKbDWDm0"
-        },
-        "Deal": {
-            "NETFARE": 13439,
-            "TDISC": 553,
-            "TDS": 28,
-            "GST": 100,
-            "DISCOUNT": {
-                "DIS": 553,
-                "SF": 0,
-                "PDIS": 0,
-                "CB": 0
-            }
         }
     },
     "HostTokens": null,

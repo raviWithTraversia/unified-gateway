76e874f4b432417c2550c71d8b253166b4e65b57
Kaushal Kishor <kkishor@traversia.net>
2024-04-23 18:08:20 +0530

    jjja

--- controllers/payuController/payu.services.js
+++ controllers/payuController/payu.services.js
@@ -13,11 +13,12 @@ const payu = async (req, res) => {
             lastName,
             email,
             mobile,
-            amount
+            amount,
+            productinfo
 
         } = req.body;
 
-        if(!companyId || !userId || !firstName || !lastName || !email || !mobile || !amount) {
+        if(!companyId || !userId || !firstName || !lastName || !email || !mobile || !amount || !productinfo) {
             return {
                 response : 'All field are required'
             }
@@ -39,10 +40,11 @@ const payu = async (req, res) => {
             email: email,
             mobile: mobile,
             amount: amount,
+            productinfo: productinfo,
             service_provide: 'test',
             call_back_url: `https://kafila.traversia.net`,
             payu_merchant_key: 'gtKFFx',
-            // payu_merchant_salt_version_1 : process.env.PAYU_MERCHANT_SALT_VERSION_1,
+            payu_merchant_salt_version_1 : '4R38IvwiV57FwVpsgOvTXBdLE4tHUXFW',
             // payu_merchant_salt_version_2 : process.env.PAYU_MERCHANT_SALT_VERSION_2,
             payu_url: 'https://test.payu.in/_payment',
             payu_fail_url: `https://kafila.traversia.net/success/url`,
@@ -51,7 +53,7 @@ const payu = async (req, res) => {
         };
         console.log(payDetails)
         // Construct the string to be hashed
-        const hashString = `${payDetails.txnId}|${payDetails.firstName}|${payDetails.lastName}|${payDetails.email}|${payDetails.mobile}|${payDetails.amount}`;
+        const hashString = `${payDetails.payu_merchant_key}|${payDetails.txnId}|${payDetails.amount}|${payDetails.productinfo}|${payDetails.firstName}|${payDetails.email}|||||||||||${payDetails.payu_merchant_salt_version_1}`;
         // Add your PayU secret key
         const secretKey = 'gtKFFx';
 

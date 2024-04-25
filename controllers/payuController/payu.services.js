3a11e7d119922ac9fe7b1fd1891774621512c486
aali@traversia.net <aali@traversia.net>
2024-04-25 17:18:21 +0530

    add payu

--- controllers/payuController/payu.services.js
+++ controllers/payuController/payu.services.js
@@ -9,8 +9,7 @@ const payu = async (req, res) => {
         const {
             companyId,
             userId,
-            firstName,
-            lastName,
+            firstName,            
             email,
             mobile,
             amount,
@@ -18,12 +17,36 @@ const payu = async (req, res) => {
 
         } = req.body;
 
-        if(!companyId || !userId || !firstName || !lastName || !email || !mobile || !amount || !productinfo) {
+        if(!companyId || !userId || !firstName || !email || !mobile || !amount || !productinfo) {
             return {
                 response : 'All field are required'
             }
         }
         
+        // const key = 'gtKFFx';
+        // const txnid = '4c14e4f2-91c6-4e4e-b942-de29e5210f5f';
+        // const amount = 500;
+        // const productinfo = 'iPhone';
+        // const firstname = 'Test';
+        // const email = 'test@example.com';
+        // const salt = '4R38IvwiV57FwVpsgOvTXBdLE4tHUXFW';
+        
+        const key = 'gtKFFx';
+        const txnid =  uuidv4();
+        const amountres = amount;
+        const productinfores = productinfo;
+        const firstnameres = firstName;
+        const emailres = email;
+        const salt = '4R38IvwiV57FwVpsgOvTXBdLE4tHUXFW';
+
+
+        // Concatenate the transaction details
+        const hashString = `${key}|${txnid}|${amountres}|${productinfores}|${firstnameres}|${emailres}|||||||||||${salt}`;
+
+        // Calculate the SHA-512 hash
+        const hash = crypto.createHash('sha512').update(hashString).digest('hex');
+    
+        payDetails = hash; // Add the hash to payment details
         // check companyId exist or not
         // const checkExistCompany = await Company.findById(companyId);
         // if(!checkExistCompany) {
@@ -31,43 +54,43 @@ const payu = async (req, res) => {
         //         response : 'companyId does not exist'
         //     }
         // }   
-        const transtionId = uuidv4();      
-        const payDetails = {
-            txnId: transtionId,
-            plan_name: "Test",
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            mobile: mobile,
-            amount: amount,
-            productinfo: productinfo,
-            service_provide: 'test',
-            call_back_url: `https://kafila.traversia.net`,
-            payu_merchant_key: 'gtKFFx',
-            payu_merchant_salt_version_1 : '4R38IvwiV57FwVpsgOvTXBdLE4tHUXFW',
-            // payu_merchant_salt_version_2 : process.env.PAYU_MERCHANT_SALT_VERSION_2,
-            payu_url: 'https://test.payu.in/_payment',
-            payu_fail_url: `https://kafila.traversia.net/success/url`,
-            payu_cancel_url: `https://kafila.traversia.net/success/url`,
-            payu_url: 'https://test.payu.in/_payment',
-        };
+        //const transtionId = uuidv4();      
+        // const payDetails = {
+        //     txnId: transtionId,
+        //     plan_name: "Test",
+        //     firstName: firstName,
+        //     lastName: lastName,
+        //     email: email,
+        //     mobile: mobile,
+        //     amount: amount,
+        //     productinfo: productinfo,
+        //     service_provide: 'test',
+        //     call_back_url: `https://kafila.traversia.net`,
+        //     payu_merchant_key: 'gtKFFx',
+        //     payu_merchant_salt_version_1 : '4R38IvwiV57FwVpsgOvTXBdLE4tHUXFW',
+        //     // payu_merchant_salt_version_2 : process.env.PAYU_MERCHANT_SALT_VERSION_2,
+        //     payu_url: 'https://test.payu.in/_payment',
+        //     payu_fail_url: `https://kafila.traversia.net/success/url`,
+        //     payu_cancel_url: `https://kafila.traversia.net/success/url`,
+        //     payu_url: 'https://test.payu.in/_payment',
+        // };
         //console.log(payDetails)
         // Construct the string to be hashed
-        const hashString = `${payDetails.payu_merchant_key}|${payDetails.txnId}|${payDetails.amount}|${payDetails.productinfo}|${payDetails.firstName}|${payDetails.email}|||||||||||$payDetails.payu_merchant_salt_version_1}`;
+        //const hashString = `${payDetails.payu_merchant_key}|${payDetails.txnId}|${payDetails.amount}|${payDetails.productinfo}|${payDetails.firstName}|${payDetails.email}|||||||||||$payDetails.payu_merchant_salt_version_1}`;
         // Add your PayU secret key
         //const secretKey = 'gtKFFx';
 
         // Create the SHA512 hash using the secret key
-        const hash = crypto.createHash('sha512');
-        hash.update(hashString);
-        const payu_sha_token = hash.digest('hex');
+        // const hash = crypto.createHash('sha512');
+        // hash.update(hashString);
+        // const payu_sha_token = hash.digest('hex');
 
         //console.log(payu_sha_token);
        
-        if (payu_sha_token) {
+        if (payDetails) {
             return {
                 response : "payU sha token generate successfully",
-                data: {payDetails:payDetails, haskKey:payu_sha_token }
+                data: payDetails 
             }
         } else {
             return {

9f3d3321d6088493815b26dd6105628aaeb2f29f
Deepanshu Tyagi <dtyagi@traversia.net>
2024-05-23 10:41:39 +0530

    don't merge this commit

--- controllers/ledget/ledger.controller.js
+++ controllers/ledget/ledger.controller.js
@@ -8,35 +8,40 @@ const {
 
 const getAllledger = async (req, res) => {
   try {
-      const result = await getAllledgerServices.getAllledger(req, res);
-      if (!result.response && result.isSometingMissing) {
-        apiErrorres(res, result.data, ServerStatusCode.SERVER_ERROR, true);
-      }else if (result.response === "User id does not exist" || result.response === "Data Not Found") {
-          apiErrorres(res, result.response, ServerStatusCode.BAD_REQUEST, true);
-      }else if (result.response === "Fetch Data Successfully") {
-        apiSucessRes(
-          res,
-          result.response,
-          result.data,
-          ServerStatusCode.SUCESS_CODE
-        );          
-      }else {
-        apiErrorres(
-          res,
-          errorResponse.SOME_UNOWN,
-          ServerStatusCode.UNPROCESSABLE,
-          true
-        );
-      }
-    } catch (error) {
-      console.error(error);
+    const result = await getAllledgerServices.getAllledger(req, res);
+    if (!result.response && result.isSometingMissing) {
+      apiErrorres(res, result.data, ServerStatusCode.SERVER_ERROR, true);
+    } else if (result.response === "User id does not exist" || result.response === "Data Not Found") {
+      apiErrorres(res, result.response, ServerStatusCode.BAD_REQUEST, true);
+    } else if (result.response === "Fetch Data Successfully") {
+      apiSucessRes(
+        res,
+        result.response,
+        result.data,
+        ServerStatusCode.SUCESS_CODE
+      );
+    } else {
       apiErrorres(
         res,
-        errorResponse.SOMETHING_WRONG,
-        ServerStatusCode.SERVER_ERROR,
+        errorResponse.SOME_UNOWN,
+        ServerStatusCode.UNPROCESSABLE,
         true
       );
     }
+  } catch (error) {
+    console.error(error);
+    apiErrorres(
+      res,
+      errorResponse.SOMETHING_WRONG,
+      ServerStatusCode.SERVER_ERROR,
+      true
+    );
+  }
 };
 
-module.exports = { getAllledger };
+const transactionReport = async (req, res) => {
+  const result = await getAllledgerServices.transactionReport(req, res);
+  return res.send(result)
+}
+
+module.exports = { getAllledger, transactionReport };

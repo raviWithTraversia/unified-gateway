54d661714c9480b17c4be9fc4209bdf695c7f7c8
dtyagittpl <dtyagi@traversia.net>
2024-05-27 10:46:23 +0530

    add count

--- controllers/layout/layout.services.js
+++ controllers/layout/layout.services.js
@@ -1,29 +1,39 @@
 const registration = require('../../models/Registration');
 const creditRequest = require('../../models/CreditRequest');
+const bookingdetails = require("../../models/booking/BookingDetails");
+const company = require("../../models/Company");
+const depositDetail = require("../../models/DepositRequest");
+
 const axios = require('axios');
 const { Config } = require("../../configs/config");
 const registrationServices = require('../../controllers/registration/registration.services')
 
-const dashBoardCount  = async (req,res) => {
-    try{
-    let  {companyId} = req.query;
+const dashBoardCount = async (req, res) => {
+  try {
+    let { companyId } = req.query;
     let data = {};
-    let req1 = {
-      params : {
-        companyId : companyId
-      }
-    };
-    let newRegistrationCount = await registrationServices.getAllRegistrationByCompany(req1,res)
-    let creditReqCount = await creditRequest.find({companyId : companyId});
-    let pending = 0;
-    let hold = 0;
-    let failedAtPayment = 0;
-    let allFailed = 0;
-    let refund = 0;
-    let refundPending = 0;
-    let RegisteredAgentConfig = 0;
-    let depositRequest = 0;
-    let amendement = 0;
+    let req1 = { params: { companyId: companyId } };
+    let fifteenDaysAgo = new Date();
+    fifteenDaysAgo.setDate(new Date().getDate() - 15);
+    console.log(fifteenDaysAgo)
+
+    let [newRegistrationCount, creditReqCount, getBookingDetails, RegisteredAgentConfig, depositRequest] = await Promise.all([
+      registrationServices.getAllRegistrationByCompany(req1, res),
+      creditRequest.find({ companyId: companyId }),
+      bookingdetails.find({ createdAt: { $gte: fifteenDaysAgo }, companyId }, { bookingStatus: 1 }),
+      company.countDocuments({ parent: companyId }),
+      depositDetail.countDocuments({ companyId, status: "pending" })
+    ]);
+
+    let pending = 0, hold = 0, failedAtPayment = 0, allFailed = 0, refund = 0, refundPending = 0, amendement = 0
+
+    getBookingDetails.map(item => {
+      if (item.bookingStatus == "PENDING") pending++;
+      if (item.bookingStatus == "HOLD") hold++;
+      if (item.bookingStatus == "FAILEDPAYMENT") failedAtPayment++;
+      if (item.bookingStatus == "FAILED") allFailed++;
+      if (item.bookingStatus == "REFUND") refund++;
+    });
 
     newRegistrationCount = newRegistrationCount?.data?.length || 0;
     creditReqCount = creditReqCount.length || 0;
@@ -39,42 +49,40 @@ const dashBoardCount  = async (req,res) => {
     data['depositRequest'] = depositRequest;
     data['amendement'] = amendement;
 
-    if(newRegistrationCount){
-        return {
-            response : "Count Fetch Sucessfully",
-            data : data
-        }
-    }else{
-        return {
-            response : 'Data Found Sucessfully' ,
-            data : data
-        }
+    if (newRegistrationCount) {
+      return {
+        response: "Count Fetch Sucessfully",
+        data
+      }
+    } else {
+      return {
+        response: 'Data Found Sucessfully',
+        data
+      }
     }
-      
-    }catch(error){
-       console.log(error);
-       throw error 
-    }
+  } catch (error) {
+    throw error
+  }
 };
 const checkPanCard = async (req, res) => {
-    try {
-      const { panNumber } = req.body;
-  
-      if (!panNumber) {
-        return {
-          response: "PAN number is required"
-        };
+  try {
+    const { panNumber } = req.body;
+
+    if (!panNumber) {
+      return {
+        response: "PAN number is required"
+      };
+    }
+
+    const apiUrl = Config.PAN_URL;
+    const headers = Config.HADDER_3RD_PAERT;
+
+    let responseData = await axios.post(apiUrl, null, {
+      headers,
+      params: {
+        pan_number: panNumber
       }
-  
-      const apiUrl = Config.PAN_URL;
-      const headers = Config.HADDER_3RD_PAERT;
-  
-      let responseData = await axios.post(apiUrl,null,{
-        headers,
-        params: {
-          pan_number: panNumber
-        }
-      });
+    });
     // let  responseData =  {
     //     "data": {
     //         "pan_number": "CNAPK1068F",
@@ -88,62 +96,62 @@ const checkPanCard = async (req, res) => {
     //         "pan_aadhaar_seeding": "Y"
     //     }
     // }
-      if(responseData){
-        return {
-          response : "Data Fetch Sucessfully",
-          data: responseData.data  
-        };
-      }else{
-          return {
-              response : "Some Error in 3rd party api"
-          }
+    if (responseData) {
+      return {
+        response: "Data Fetch Sucessfully",
+        data: responseData.data
+      };
+    } else {
+      return {
+        response: "Some Error in 3rd party api"
       }
-    } catch (error) {
-      throw error;
     }
+  } catch (error) {
+    throw error;
+  }
 };
-const checkGstin = async (req,res) => {
-    try{
-        const { gstNumber } = req.body;
-       
-      if (!gstNumber) {
-        return {
-          response: "GST number is required"
-        };
-      }
-  
-      const apiUrl = Config.GST_URL;
-      const headers = Config.GST_TOKEN;
+const checkGstin = async (req, res) => {
+  try {
+    const { gstNumber } = req.body;
+
+    if (!gstNumber) {
+      return {
+        response: "GST number is required"
+      };
+    }
+
+    const apiUrl = Config.GST_URL;
+    const headers = Config.GST_TOKEN;
     //  console.log(apiUrl,"<<<==========", headers, "Code is here 114")
-      const responseData = await axios.post(
-        apiUrl,
-        null,
-        {
+    const responseData = await axios.post(
+      apiUrl,
+      null,
+      {
         headers,
         params: {
           gst_number: gstNumber
         }
       });
     // console.log(apiUrl,"<<<==========", responseData.data.data, "Code is here 121")
-      if(responseData){
+    if (responseData) {
       return {
-        response : "Data Fetch Sucessfully",
-        data: responseData.data  
+        response: "Data Fetch Sucessfully",
+        data: responseData.data
       };
-    }else{
-        return {
-            response : "Some Error"
-        }
+    } else {
+      return {
+        response: "Some Error"
+      }
     }
-    }
-    catch(error){
-        console.log(error);
-        throw error 
-    }
+  }
+  catch (error) {
+    console.log(error);
+    throw error
+  }
 };
 
 module.exports = {
-    dashBoardCount,
-    checkPanCard,
-    checkGstin
+  dashBoardCount,
+  checkPanCard,
+  checkGstin
 }

bed7b709f27122e8244054ddbb2c8a1a3c61ba23
dtyagittpl <dtyagi@traversia.net>
2024-06-03 15:27:10 +0530

    create railSearch api

---
+++ controllers/rail/railBooking.services.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+const getRailSearch = async (req, res) => {
+    try {
+        const { from, to, date } = req.body;
+        if (!from, !to, !date) { return { response: "Provide required fields" } };
+        let renewDate = date.split("-");
+        const url = `http://43.205.65.20:8000/eticketing/webservices/taenqservices/tatwnstns/${from}/${to}/${renewDate[0]}${renewDate[1]}${renewDate[2]}`;
+        const auth = 'Basic V05FUFRQTDAwMDAwOlRlc3Rpbmcx';
+        const response = (await axios.get(url, { headers: { 'Authorization': auth } }))?.data;
+        if (!response?.trainBtwnStnsList?.length) {
+            return {
+                response: "Error in fetching data",
+            }
+        } else {
+            return {
+                response: "Fetch Data Successfully",
+                data: response,
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        apiErrorres(
+            res,
+            errorResponse.SOMETHING_WRONG,
+            ServerStatusCode.SERVER_ERROR,
+            true
+        );
+    }
+}
+
+module.exports = { getRailSearch }
---
+++ routes/railRoute.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const rail_route = express();
+const bodyParser = require("body-parser");
+rail_route.use(bodyParser.json());
+rail_route.use(bodyParser.urlencoded({ extended: true }));
+const auth = require("../middleware/auth");
+const railController = require('./../controllers/rail/rail.controller');
+
+rail_route.get('/rail/railSearch', auth, railController.railSearch);
+// easeBuzz_route.post('/paymentGateway/easeBussResponce', auth, easeBuzzController.easeBuzzResponce);
+
+module.exports = rail_route;

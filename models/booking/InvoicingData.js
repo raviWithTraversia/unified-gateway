fdb74ff6e3dba58007b05ec0fc04cbde044b4001
Shadaab Ali <sali@traversia.net>
2024-07-17 18:04:27 +0530

    generateInvoice api

---
+++ models/booking/InvoicingData.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const InvoicingDataSchema = new mongoose.Schema({ 
+    invoiceNumber:{
+        type:String,
+        default:null
+    },
+    bookingId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "BookingDetails",
+    },
+    productId:{
+        type: mongoose.Schema.Types.ObjectId,
+        default:null
+    },
+    passenger:{
+        type: Object,  
+        required: true
+    },
+    generatedByuserID:{
+        type: mongoose.Schema.Types.ObjectId,
+        default:null
+        // ref: "User",
+    },
+    companyId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Company",
+    },
+    AgencyId:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }]
+},{
+    timestamps : true  
+});
+
+module.exports = mongoose.model('InvoicingData' , InvoicingDataSchema);

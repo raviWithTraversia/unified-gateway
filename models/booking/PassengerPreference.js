8827023f07f936c8f2fa487548cf8a9deaac1e1a
mkumar-traversia <mkumar@traversia.net>
2024-08-08 12:05:04 +0530

    changes in booking Gst isAgentGst

--- models/booking/PassengerPreference.js
+++ models/booking/PassengerPreference.js
@@ -23,7 +23,8 @@ const passengerPreferenceSchema = new mongoose.Schema({
     gstEmail: { type: String, default: null },
     gstAddress: { type: String, default: null },
     GSTState : {type: String, default:""},
-    GSTPinCode : {type: String, default:""}
+    GSTPinCode : {type: String, default:""},
+    isAgentGst : { type : Boolean,default : false}    
   },
   PaxEmail: { type: String, default: null },
   PaxMobile: { type: String, default: null },

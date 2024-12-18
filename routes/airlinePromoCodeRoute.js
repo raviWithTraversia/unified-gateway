c44f54787c2751e59f12531202c094241539e4c4
Mnk242 <rsharma@traversia.net>
2024-12-18 15:01:26 +0530

    Fix: pass userId in params in get promo code

--- routes/airlinePromoCodeRoute.js
+++ routes/airlinePromoCodeRoute.js
@@ -2,37 +2,37 @@ const express = require("express");
 const airlinePromoCode_route = express();
 const bodyParser = require("body-parser");
 airlinePromoCode_route.use(bodyParser.json());
-airlinePromoCode_route.use(bodyParser.urlencoded({extended:true}));
+airlinePromoCode_route.use(bodyParser.urlencoded({ extended: true }));
 const auth = require("../middleware/auth");
-const airlinePromoController = require('./../controllers/airLinePromoCode/airLinePromoCode.controller');
+const airlinePromoController = require("./../controllers/airLinePromoCode/airLinePromoCode.controller");
 
 airlinePromoCode_route.post(
-    '/airlinePromocode/addPromo',
-    auth,
-    airlinePromoController.addAirlinePromoCode
+  "/airlinePromocode/addPromo",
+  auth,
+  airlinePromoController.addAirlinePromoCode
 );
 airlinePromoCode_route.patch(
-   '/airlinePromocode/editPromo',
-   auth,
-   airlinePromoController.editAirlinePromoCode
+  "/airlinePromocode/editPromo",
+  auth,
+  airlinePromoController.editAirlinePromoCode
 );
 airlinePromoCode_route.get(
-    '/airlinePromocode/getPromo',
-    auth,
-    airlinePromoController.getPromoCode
+  "/airlinePromocode/getPromo/:userId",
+  // auth,
+  airlinePromoController.getPromoCode
 );
 airlinePromoCode_route.get(
-    '/airlineCompanyPromocode/getPromo',
-    auth,
-    airlinePromoController.getCompanBasePromoCode
+  "/airlineCompanyPromocode/getPromo",
+  auth,
+  airlinePromoController.getCompanBasePromoCode
 );
 airlinePromoCode_route.delete(
-    '/airlinePromocode/deletePromo',
-    auth,
-    airlinePromoController.deletePromoCode
-)
-airlinePromoCode_route.get('/test',auth, function(req, res){
-    res.status(200).json({status:"success",msg:"this is test responce"});
+  "/airlinePromocode/deletePromo",
+  auth,
+  airlinePromoController.deletePromoCode
+);
+airlinePromoCode_route.get("/test", auth, function (req, res) {
+  res.status(200).json({ status: "success", msg: "this is test responce" });
 });
 
 module.exports = airlinePromoCode_route;

394be9836157758043524caef6426e8b02b4bf8c
Mnk242 <rsharma@traversia.net>
2025-01-17 18:06:40 +0530

    Config: additionalFlightsBaseURL

--- configs/config.js
+++ configs/config.js
@@ -5,7 +5,7 @@ module.exports.Config = {
     baseURL: "https://kafilaui.traversia.net", // frontend
     baseURLBackend: "https://kafila.traversia.net",
     // additionalFlightsBaseURL: "http://localhost:31101/api",
-    additionalFlightsBaseURL: "http://kflapi.kafilaholidays.in:61101/api",
+    additionalFlightsBaseURL: "http://kafilaapi.traversia.net:61101/api",
     baseURLBackend: "https://kafila.traversia.net",
     IRCTC_BASE_URL: "https://stagews.irctc.co.in",
     IRCTC_MASTER_ID: "WKAFL00000",
@@ -15,7 +15,7 @@ module.exports.Config = {
   LIVE: {
     baseURL: "https://agent.kafilaholidays.in", // frontend
     baseURLBackend: "https://agentapi.kafilaholidays.in",
-    additionalFlightsBaseURL: "http://kflapi.kafilaholidays.in:61101/api",
+    additionalFlightsBaseURL: "http://kafilaapi.traversia.net:61101/api",
     baseURLBackend: "https://agentapi.kafilaholidays.in",
     IRCTC_BASE_URL: "https://www.aps.irctc.co.in",
     IRCTC_MASTER_ID: "WKAFILA00000",

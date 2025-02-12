1496f02477287c3b14df737a531b59dfbd497998
RS <rsharma@traversia.net>
2025-02-12 19:13:30 +0530

    Fix: correct cancellation URL

--- services/common-air-cancellation.js
+++ services/common-air-cancellation.js
@@ -15,13 +15,16 @@ module.exports.commonAirBookingCancellation = async function (request) {
     if (error) return { error };
     saveLogInFile("cancellation-request.json", requestBody);
     const url =
-      Config[request?.SearchRequest?.Authentication?.CredentialType ?? "TEST"]
+      Config[request?.Authentication?.CredentialType ?? "TEST"]
         .additionalFlightsBaseURL + "/cancel/cancelPNR";
 
     const { data: response } = await axios.post(url, requestBody);
+    saveLogInFile("cancellation-response.json", response);
+
     if (response?.errors?.length) return { error: response.errors[0] };
     return { result: response?.data };
   } catch (error) {
+    saveLogInFile("cancellation-error.json", error?.response?.data);
     console.dir({ response: error?.response?.data }, { depth: null });
     return { error };
     // throw new Error(error.message);
